43e2c9575a1b6065dc03f354af99cea87bf6c951
Lobod <konta.lobod@o2.pl>
2018-10-27 21:50:31 +0200

    Serverside changes

--- server.js
+++ server.js
@@ -19,21 +19,21 @@ io.on("connect", (socket) => {
             socket.broadcast.emit("Wing-Status", {
                 "name": user,
                 "status": "Online"
-            })
+            });
             socket.on("check-wing", (data) => {
                 console.dateLog(user+" requested wing check");
                 data=JSON.parse(data);
                 for (var i = 0; i < data.length; i++) {
                     console.log("Checking:"+data[i]+": "+ userConnected.indexOf(data[i]))
                     if (userConnected.indexOf(data[i]) >= 0){
-                        socket.broadcast.emit("Wing-Status", {
+                        socket.emit("Wing-Status", {
                             "name": data[i],
                             "status": "Online"
                         });
                     }   
                 }
             });
-            socket.on('disconnect', function () {
+            socket.on('disconnect', function (socket) {
                 index = userConnected.indexOf(user);
                 userConnected.splice(index, 1);
                 socket.broadcast.emit("Wing-Status", {
